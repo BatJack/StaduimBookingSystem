e79489852c192f1b8c38435c11037f2951e887b0
jackson23who <jackson23who@gmail.com>
2026-04-18 23:13:06 +0800

    Allow other device access

--- stadium_booking/settings.py
+++ stadium_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vyvu_t_39q#6lj_1ws0bv1gb_%m#iior%v)^mkjp+#*9vmri04
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
